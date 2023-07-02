fc95552a0fb0fd278d4e8709ad23859b95faa3ab
VitMarKha <vitmarkha@icloud.com>
2023-07-02 20:00:10 +0300

    Designed the main screen with the first joke

--- JokeGenerator/ViewController.swift
+++ JokeGenerator/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Firs stack
+    @IBOutlet private weak var jokeIdStackView: UIStackView!
+    @IBOutlet private weak var jokeIdTextLabel: UILabel!
+    @IBOutlet private weak var jokeIdLabel: UILabel!
 
+    // Second stack
+    @IBOutlet private weak var typeStackView: UIStackView!
+    @IBOutlet private weak var typeTextLabel: UILabel!
+    @IBOutlet private weak var typeNameLabel: UILabel!
+    
+    // Third stack
+    @IBOutlet private weak var jokeStackView: UIStackView!
+    @IBOutlet private weak var setupTextLabel: UILabel!
+    @IBOutlet private weak var jokeInsideStackView: UIStackView!
+    @IBOutlet private weak var borderTopView: UIView!
+    @IBOutlet private weak var jokeTextLabel: UILabel!
+    @IBOutlet weak var borderButtonView: UIView!
+    
+    @IBOutlet weak var buttonStackView: UIStackView!
+    @IBOutlet weak var reloadJokeButton: UIButton!
+    @IBOutlet weak var showPunchlineButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // First stack config
+        jokeIdStackView.isLayoutMarginsRelativeArrangement = true
+        jokeIdStackView.directionalLayoutMargins.leading = 24
+        jokeIdStackView.directionalLayoutMargins.trailing = 24
+        jokeIdStackView.layer.cornerRadius = 8
+        jokeIdStackView.layer.borderWidth = 2
+        jokeIdStackView.layer.borderColor = UIColor.black.cgColor
+        
+        // Second stack config
+        typeStackView.isLayoutMarginsRelativeArrangement = true
+        typeStackView.directionalLayoutMargins.leading = 24
+        typeStackView.directionalLayoutMargins.trailing = 24
+        typeStackView.layer.cornerRadius = 8
+        typeStackView.layer.borderWidth = 2
+        typeStackView.layer.borderColor = UIColor.black.cgColor
+        
+        // Third stack config
+        jokeStackView.isLayoutMarginsRelativeArrangement = true
+        jokeStackView.directionalLayoutMargins.leading = 24
+        jokeStackView.directionalLayoutMargins.trailing = 24
+        jokeStackView.directionalLayoutMargins.top = 24
+        jokeStackView.directionalLayoutMargins.bottom = 24
+        jokeStackView.layer.cornerRadius = 8
+        jokeStackView.layer.borderWidth = 2
+        jokeStackView.layer.borderColor = UIColor.black.cgColor
+        
+        // Fore stack config
+        buttonStackView.isLayoutMarginsRelativeArrangement = true
+        reloadJokeButton.layer.cornerRadius = 8
+        reloadJokeButton.layer.borderWidth = 2
+        reloadJokeButton.layer.borderColor = UIColor.black.cgColor
+        showPunchlineButton.layer.cornerRadius = 8
+        showPunchlineButton.layer.borderWidth = 2
+        showPunchlineButton.layer.borderColor = UIColor.black.cgColor
     }
-
-
+    
+    @IBAction private func showPunchlineAction(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Punchline",
+                                       message: "For fowl play",
+                                       preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok",
+                                   style: .default)
+        
+        alert.addAction(action)
+        super.present(alert, animated: true)
+    }
 }
 
